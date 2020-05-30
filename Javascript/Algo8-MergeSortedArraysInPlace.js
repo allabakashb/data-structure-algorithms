038a38a9d8ca01b372c73974a85d49a8d0430e33
allabakashb <allabakash0@gmail.com>
2020-05-31 00:36:16 +0530

    Update Algo8-MergeSortedArraysInPlace.js

--- Javascript/Algo8-MergeSortedArraysInPlace.js
+++ Javascript/Algo8-MergeSortedArraysInPlace.js
@@ -43,3 +43,40 @@ function mergeArrays(arr1, arr2) {
   Time Complexity - O(M+N)
   Space Complexity - O(1)
 */
+
+
+//Brute Force
+
+function mergeArrays(arr1, arr2) {
+
+	for (let i = 0; i < arr2.length; i++) {
+	
+		for (let j = 0; j < arr1.length; j++) {
+		
+			if (arr2[i] < arr1[j] || arr1[j] == 0) {
+				
+				let k = j + 1, prev = arr1[j];
+				while (k < arr1.length) {
+				
+					let temp = arr1[k];
+					arr1[k++] = prev;
+					prev = temp;
+					
+					if (prev == 0) break;
+				}
+				
+				arr1[j] = arr2[i];
+				break;
+			}
+		}
+	}
+	
+	console.log(arr1);
+}
+
+/*
+
+ Time Complexity - O(M * N)
+ Space Complexity - O(1)
+
+*/
