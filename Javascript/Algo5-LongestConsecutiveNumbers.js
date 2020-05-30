6214886ad93ecb2cc3403d34eca054562f26b34d
allabakashb <allabakash0@gmail.com>
2020-05-30 22:07:37 +0530

    Update Algo5-LongestConsecutiveNumbers.js

--- Javascript/Algo5-LongestConsecutiveNumbers.js
+++ Javascript/Algo5-LongestConsecutiveNumbers.js
@@ -37,3 +37,55 @@ function getConsecutiveNumbers(array) {
    Time Complexity - O(N^2);
    Space Complexity - O(N);
 */
+
+
+// Sorting
+function getConsecutiveNumbers(array) {
+
+	let result = []; 
+	
+	array.sort((n1, n2) => n1 - n2);
+	
+	let start = 0, end = 0, tempS = 0, tempE = 0, prev = array[0];
+	for (let i = 1; i < array.length; i++) {
+	
+		if (prev == array[i] || array[i] - prev == 1) {
+		
+			tempE++;
+		} else {
+		
+			if (tempE-tempS > end-start) {
+				
+				start = tempS;
+				end = tempE;
+			}
+			
+			++tempE;
+			tempS = tempE;
+		}
+		
+		if (tempE-tempS > end-start) {
+				
+			start = tempS;
+			end = tempE;
+		}
+			
+		prev = array[i];
+	}
+	
+	let set = new Set();
+	
+	for (let i = start; i <= end; i++) {
+		
+		set.add(array[i]);
+	}
+	
+	return set;
+}
+
+/*
+
+Time Complexity - O(NlogN)
+Space Complexity - O(N)
+
+*/
