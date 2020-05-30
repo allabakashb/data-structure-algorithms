81672a645ad907288165924e809ca02db137ff4a
allabakashb <allabakash0@gmail.com>
2020-05-30 23:07:46 +0530

    Update Algo6-ZeroMatrix.js

--- Javascript/Algo6-ZeroMatrix.js
+++ Javascript/Algo6-ZeroMatrix.js
@@ -16,11 +16,11 @@
 
   OUTPUT =>
   
-[[true, false, false, false],
-[false, false, false, false],
-[false, false, true, false],
-[false, true, false, false],
-[false, false, false, false]]
+[[true, true, true, false],
+[true, true, true, false],
+[true, true, true, true],
+[true, true, true, true],
+[true, true, true, false]]
 */
 
 /*----------------------------------Approach 1---------------------------*/
@@ -61,4 +61,69 @@ function zeroMatrix(matrix) {
 	return matrix;
 }
 
+/*
+
+Time Complexity - O(M*N)
+Space Complexity - O(M+N)
+
+*/
+
 /*----------------------------------Approach 1---------------------------*/
+
+/*----------------------------------Approach 2---------------------------*/
+// Efficient Approach
+function zeroMatrix(matrix) {
+
+	let firstRow = false, firstCol = false;
+	
+	for (let i = 0; i < matrix.length; i++) {
+	
+		for (let j = 0; j < matrix[i].length; j++) {
+	
+			if (matrix[i][j]) {
+				
+				if (i == 0) firstRow = true;
+				if (j == 0) firstCol = true;
+				
+				matrix[i][0] = true;
+				matrix[0][j] = true;
+			}
+		}
+	}
+	
+	for (let i = 1; i < matrix.length; i++) {
+	
+		for (let j = 1; j < matrix[i].length; j++) {
+		
+			if (matrix[i][0] || matrix[0][j]) {
+				
+				matrix[i][j] = true;
+			}
+		}	
+	}
+	
+	if (firstRow) {
+	
+		for (let j = 0; j < matrix[0].length; j++) {
+			
+			matrix[0][j] = true;
+		}
+	}
+	
+	if (firstCol) {
+	
+		for (let i = 0; i < matrix.length; i++) {
+			
+			matrix[i][0] = true;
+		}
+	}
+
+	return matrix;
+}
+
+/*
+
+   Time Complexity - O(M * N)
+   Space Complexity - O(1)
+*/
+/*----------------------------------Approach 2---------------------------*/
